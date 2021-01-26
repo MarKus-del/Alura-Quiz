b7c99a46748708bb73dc96198a6028acbc2837ee
MarKus-del <marcos.rodrigues.ft@gmail.com>
2021-01-26 15:42:21 -0300

    finished day 2

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget'
-import Footer from '../src/components/Footer'
-import GithubCorner from '../src/components/GithubCorner'
-import QuizBackground from '../src/components/QuizBackground'
-import QuizLogo from '../src/components/QuizLogo'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-position: center;
-  background-size: cover;
-`;
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GithubCorner from '../src/components/GithubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizLogo from '../src/components/QuizLogo';
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -25,9 +22,25 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-  return(
+  const router = useRouter();
+  const [name, setName] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    router.push(`/quiz?name=${name}`);
+  }
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Home</title>
+      </Head>
       <QuizContainer>
+        <QuizLogo />
 
         <Widget>
           <Widget.Header>
@@ -36,9 +49,24 @@ export default function Home() {
 
           <Widget.Content>
             <p>{db.description}</p>
-          </Widget.Content>     
+
+            <form onSubmit={handleSubmit}>
+              <input
+                placeholder="Diz ai seu nome para jogar :)"
+                onChange={handleChange}
+              />
+              <button
+                type="submit"
+                disabled={name.length === 0}
+              >
+                Jogar
+                {' '}
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
         </Widget>
-         
+
         <Widget>
           <Widget.Header>
             <h1>Quizes da Galera</h1>
@@ -46,12 +74,12 @@ export default function Home() {
 
           <Widget.Content>
             <p>orem ipsum dolor sit amet...</p>
-          </Widget.Content>        
+          </Widget.Content>
         </Widget>
         <Footer />
 
       </QuizContainer>
-      <GithubCorner projectUrl="https://github.com/MarKus-del"/>
-    </ QuizBackground>  
-  ); 
+      <GithubCorner projectUrl="https://github.com/MarKus-del" />
+    </QuizBackground>
+  );
 }
